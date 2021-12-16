8aa1de826113f292b48a7f2200c398a8540c2587
dariadar <savchenko22daria22@gmail.com>
2021-12-17 01:23:50 +0200

    secondcommit

--- script.js
+++ script.js
@@ -1,5 +1,51 @@
-let navbar = document.querySelector('.center_row');
+/* window.onload = function(){ 
+    let navbar = document.querySelector('.center_row');
 
-document.querySelector('#menu-btn').onclick = () => {
+    document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
+    };
+}
+*/
+var isMobile = {
+    Android: function () {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function () {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function () {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function () {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function () {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function () {
+        return (
+                isMobile.Android()
+                || isMobile.BlackBerry()
+                || isMobile.iOS()
+                || isMobile.Opera()
+                || isMobile.Windows()
+                );
+    }
+};
+if (isMobile.any()) {
+    document.body.classList.add('_mobile');
+    let menuArrows = document.querySelector('.menu_arrow');
+    document.querySelector('.menu_arrow').onclick = () =>{
+    menuArrows.parentElement.classList.toggle('active');}
+}
+else {
+    document.body.classList.add('_pc');
+}
+
+const iconMenu = document.querySelector('.menu_icon');
+const menuBody = document.querySelector('.nav_container');
+document.querySelector('.menu_icon').onclick = () =>{
+    document.body.classList.toggle('lock');
+    menuBody.classList.toggle('active');
+    iconMenu.classList.toggle('active');
 }
