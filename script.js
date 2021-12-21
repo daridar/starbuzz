5abb260be4933254dff3a876319968ea65d79568
dariadar <savchenko22daria22@gmail.com>
2021-12-21 03:37:29 +0200

    4thcommit

--- script.js
+++ script.js
@@ -56,3 +56,151 @@ document.querySelector('.menu_icon').onclick = () =>{
         item.addEventListener('click', ()=>
         item.parentNode.dataset.totalValue = item.dataset.itemValue)
         );
+//popup
+
+
+
+
+
+
+
+
+
+
+//sumup prices
+let carts = document.querySelectorAll('.add-cart');
+
+let products = [
+    {   
+        name: 'Blonde Espresso',
+        tag: 'blonde',
+        price: 155,
+        inCart: 0
+    },
+    {   
+        name: 'Holiday Blend',
+        tag: 'holiday',
+        price: 144,
+        inCart: 0
+    },
+    {   
+        name: 'House Blend',
+        tag: 'house',
+        price: 160,
+        inCart: 0
+    },
+    {   
+        name: 'Espresso Roast',
+        tag: 'roast blend',
+        price: 175,
+        inCart: 0
+    }
+];
+
+for (let i = 0; i < carts.length; i++){
+    carts[i].addEventListener('click', () => {
+        cartNumbers(products[i]);
+        totalCost(products[i]);
+    });
+}
+
+function onLoadCartNumbers(){
+    let productNumbers = localStorage.getItem('cartNumbers');
+
+    if(productNumbers){
+        document.querySelector('.shopping span').textContent = productNumbers;
+    }
+}
+
+function cartNumbers(product){
+    let productNumbers = localStorage.getItem('cartNumbers');
+
+    productNumbers = parseInt(productNumbers);
+
+    if(productNumbers){
+        localStorage.setItem('cartNumbers', productNumbers + 1);
+        document.querySelector('.shopping span').textContent = productNumbers + 1;
+    } else{
+        localStorage.setItem('cartNumbers', 1);
+        document.querySelector('.shopping span').textContent = 1;
+    }
+    setItems(product);
+}
+function setItems(product){
+    let cartItems = localStorage.getItem('productsInCart');
+    cartItems = JSON.parse(cartItems);
+
+    if(cartItems != null){
+        if(cartItems[product.tag] == undefined){
+            cartItems = {
+                ...cartItems,
+                [product.tag]: product
+            }
+        }
+        cartItems[product.tag].inCart += 1;
+    } else {
+        product.inCart = 1;
+    cartItems = {
+        [product.tag]: product
+    }
+}
+    
+    localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+}
+
+function totalCost(product){
+    //console.log("the products price is", product.price);
+    let cartCost = localStorage.getItem('totalCost');
+    console.log("my cartCost is", cartCost);
+
+    if(cartCost != null){
+        cartCost = parseInt(cartCost);
+        localStorage.setItem('totalCost', cartCost + product.price);
+    }
+    else{
+        localStorage.setItem('totalCost', product.price);
+    }
+}
+
+function displayCart(){
+    let cartItems = localStorage.getItem('productsInCart');
+    cartItems = JSON.parse(cartItems);
+    let productContainer = document.querySelector('.products');
+    let cartCost = localStorage.getItem('totalCost');
+
+    console.log(cartItems)
+    if(cartItems && productContainer){
+        productContainer.innerHTML = '';
+        Object.values(cartItems).map(item => {
+            productContainer.innerHTML += `
+            <div class="product">
+                <img src="./img/${item.tag}.png" style="height:150px; width:150px">
+                <p>${item.name}</p>
+            </div>
+            <div class="price">₴${item.price}</div>
+            <div class="quantity">
+            <img src="./img/arrow-left.png">
+                <span>${item.inCart}</span>
+                <img src="./img/arrow-right.png">
+            </div>
+            <div class="total">
+            ₴${item.inCart * item.price}
+            </div>
+            `;
+        });
+
+        productContainer.innerHTML += `
+        <div class="basketTotalContainer">
+            <h4 class="basketTotalTitle">
+                Усього:
+            </h4>
+            <h4 class="basketTotal">
+                ₴${cartCost}
+            </h4>
+        </div>
+        `
+    }
+}
+
+onLoadCartNumbers();
+displayCart();
